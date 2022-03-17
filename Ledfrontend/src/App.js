44dcbfdb88d91f284e3741f74deddc139740f3fd
Tabot Charles Bessong <ebezebeatrice@gmail,com>
2022-03-17 15:14:45 +0100

    engage with us section

--- Ledfrontend/src/App.js
+++ Ledfrontend/src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import './index.css'
 import Navbar from './component/Navbar/Navbar';
 import Home from './pages/home/Home';
+// import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
+import {BrowserRouter as Router , Switch , Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Navbar/>
+        <Switch>
+
+        </Switch>
+      </Router>
       <Home/>
     </div>
   );
--- Ledfrontend/src/pages/home/Home.js
+++ Ledfrontend/src/pages/home/Home.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Banner from '../../component/banner/Banner'
+import Engage from '../../component/engage/Engage'
 import Impact from '../../component/impact/Impact'
 import Mission from '../../component/mission/Mission'
 import Programs from '../../component/programs/Programs'
@@ -10,9 +11,14 @@ const Home = () => {
   return (
     <div className='home' >
       <Banner/>
+      <hr />
       <Mission/>
+      <hr />
       <Programs/>
+      <hr />
       <Impact/>
+      <hr />
+      <Engage/>
       
     </div>
   )
