abb05cf51450c5f0e9738f9a97e9d323e1994a4a
Nerus-Zaumu <neruszaumuy@gmail.com>
2022-03-18 10:53:44 +0100

    Added JWT to user login

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const verifyToken = require('../middleware/auth')
 
 const {
     login,
-    addPost,
-    updatePost,
-    deletePost,
-    getSinglePost,
-    getAllPosts,
-    donationHistory
+    donate,
+    fillAdminDashboard
 } = require('../controllers/user')
 
 const router = express.Router();
 
+//middleware
+router.use('/admin-dashboard', verifyToken)
 
+router.route('/donate', donate)
+router.route('/admin-dashboard', fillAdminDashboard)
 router.route('/login').post(login)
 
 
