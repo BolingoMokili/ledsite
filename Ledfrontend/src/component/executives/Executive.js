b30efdfd5d2a9b5759f8104be4c8f9a5bfe6fb24
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-20 18:39:29 +0100

    corrected mistakes in the executives section

--- Ledfrontend/src/component/executives/Executive.js
+++ Ledfrontend/src/component/executives/Executive.js
@@ -3,28 +3,44 @@
 import React from 'react'
 // import {executives} from '../../constant/data'
 import data from '../../constant/data'
+import './executive.css'
 
 const Executive = () => {
   return (
     <div className='executives' >
        {
          data.executives.map((exe, index)=>{
+           return(
+          <div className="main">
            <div className="executive"  key={exe.id} >
              <div className="pic">
+              <div className="rect"></div>
                <img src={exe.img} alt={exe.lastName} />
              </div>
              <div className="text">
-               <h5>
+               <h6 className='names' >
                  <span className="fName">{exe.firstName}</span>
                  <span className="lName">{exe.lastName}</span>
-               </h5>
-               <h6>
+               </h6>
+               <h6 className='pos' >
                  {exe.post}
                </h6>
              </div>
+             
            </div>
-         })
+          </div>
+         )})
        }
+       {/* <h1>Hello world</h1> */}
+       {/* <div className="pic">
+         <img src={img} alt="" />
+       </div>
+       <div className="text">
+         <h6 className='names' >
+           <span className="fn">{firstName}</span>
+           <span className="ln">{lastName}</span>
+         </h6>
+       </div> */}
     </div>
   )
 }
