ff58580453ac0fd071a89796683fd6d5650314e8
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-21 14:59:10 +0100

    did some working for an extra layer

--- Ledfrontend/src/pages/what/What.js
+++ Ledfrontend/src/pages/what/What.js
@@ -6,19 +6,24 @@ import './what.css'
 const What = () => {
   return (
     <div className='what' >
+    <h6>our programs</h6>
+    <div className="container">
+
       {
         data.programs.map((pro)=>{
           return (
             <div className="program" key={pro.id} >
               <img src={pro.img} alt={pro.title} className="img" />
               <div className="text">
-                <h5>{pro.title}</h5>
+                <h6>{pro.title}</h6>
                 <p>{pro.desc}</p>
               </div>
             </div>
           )
         })
       }
+    </div>
+
     </div>
   )
 }
